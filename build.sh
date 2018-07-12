9021ae221939b837559892cae053d9267b4989af
fabiopina <fpina@student.dei.uc.pt>
2018-07-12 16:35:50 +0100

    changed services version

--- build.sh
+++ build.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
 # Delete images
-docker rmi fabiopina151/users-ms:0.1
-docker rmi fabiopina151/songs-ms:0.1
-docker rmi fabiopina151/playlists-ms:0.1
-docker rmi fabiopina151/aggr-ms:0.1
-docker rmi fabiopina151/auth-ms:0.1
+docker rmi fabiopina151/users-ms:latest
+docker rmi fabiopina151/songs-ms:latest
+docker rmi fabiopina151/playlists-ms:latest
+docker rmi fabiopina151/aggr-ms:latest
+docker rmi fabiopina151/auth-ms:latest
 
 
 # Aggregator
 cd Aggregator_MS
-docker build -t fabiopina151/aggr-ms:0.1 .
+docker build -t fabiopina151/aggr-ms:latest .
 cd ..
 
 # Authentication
 cd Authentication_MS
-docker build -t fabiopina151/auth-ms:0.1 .
+docker build -t fabiopina151/auth-ms:latest .
 cd ..
 
 # Playlists
 cd Playlists_MS
-docker build -t fabiopina151/playlists-ms:0.1 .
+docker build -t fabiopina151/playlists-ms:latest .
 cd ..
 
 # Songs
 cd Songs_MS
-docker build -t fabiopina151/songs-ms:0.1 .
+docker build -t fabiopina151/songs-ms:latest .
 cd ..
 
 # Users
 cd Users_MS
-docker build -t fabiopina151/users-ms:0.1 .
+docker build -t fabiopina151/users-ms:latest .
 cd ..
 
 
 # Upload images to docker hub
-docker push fabiopina151/aggr-ms:0.1
-docker push fabiopina151/auth-ms:0.1
-docker push fabiopina151/playlists-ms:0.1
-docker push fabiopina151/songs-ms:0.1
-docker push fabiopina151/users-ms:0.1
+docker push fabiopina151/aggr-ms:latest
+docker push fabiopina151/auth-ms:latest
+docker push fabiopina151/playlists-ms:latest
+docker push fabiopina151/songs-ms:latest
+docker push fabiopina151/users-ms:latest
 
 
 
